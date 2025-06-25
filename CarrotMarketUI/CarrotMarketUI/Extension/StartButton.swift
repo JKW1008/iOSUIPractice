9d30d36281a7ecbe1063a20d726e76a542f53030
JKW1008 <todendvv91@gmail.com>
2025-06-25 21:56:21 +0900

    [Feat] 시작하기 버튼 생성 완

--- CarrotMarketUI/CarrotMarketUI/Extension/StartButton.swift
+++ CarrotMarketUI/CarrotMarketUI/Extension/StartButton.swift
@@ -7,4 +7,27 @@
 
 import UIKit
 
-extension 
+extension UIView {
+    func addStartButton(bottomSpacingRatio: CGFloat = 0.08) -> UIButton {
+        let startButton = UIButton(type: .system)
+        startButton.setTitle("시작하기", for: .normal)
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.titleLabel?.font = .systemFont(ofSize: self.bounds.width * 0.05, weight: .bold)
+        startButton.backgroundColor = .carrotOrange
+        startButton.layer.cornerRadius = 10
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(startButton)
+        
+        let buttonHeight: CGFloat = self.bounds.height * 0.06
+        let bottomSpacing = self.bounds.height * bottomSpacingRatio
+        
+        NSLayoutConstraint.activate([
+            startButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            startButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -bottomSpacing),
+            startButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.9),
+            startButton.heightAnchor.constraint(equalToConstant: buttonHeight)
+        ])
+        return startButton
+    }
+}
