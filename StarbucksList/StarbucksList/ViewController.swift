8918bf6fa156b708ac3eeb7e8710b6215af9b81b
JKW1008 <todendvv91@gmail.com>
2025-07-19 14:38:46 +0900

    [Init] create project

---
+++ StarbucksList/StarbucksList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarbucksList
+//
+//  Created by 정강우 on 7/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StarbucksList/StarbucksListTests/StarbucksListTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksListTests.swift
+//  StarbucksListTests
+//
+//  Created by 정강우 on 7/19/25.
+//
+
+import Testing
+@testable import StarbucksList
+
+struct StarbucksListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
