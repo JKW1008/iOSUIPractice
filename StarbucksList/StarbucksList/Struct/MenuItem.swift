e617ad95eddc8a1b0bee09ebc876bd55a93ea029
JKW1008 <todendvv91@gmail.com>
2025-07-23 21:23:45 +0900

    [Init] MenuItem Struct Init

--- StarbucksList/StarbucksList/Struct/MenuItem.swift
+++ StarbucksList/StarbucksList/Struct/MenuItem.swift
@@ -5,4 +5,33 @@
 //  Created by 정강우 on 7/23/25.
 //
 
-import Foundation
+import UIKit
+
+struct ProductItem{
+    let id: String
+    let name: String
+    let englishName: String
+    let price: Int
+    let imageName: String
+    
+    let statusBadge: StatusBadge?
+    let hasHotIcon: Bool
+    
+    var formattedPrice: String {
+        return "\(price.formatted())원"
+    }
+}
+
+enum StatusBadge: String {
+    case new = "New"
+    case best = "Best"
+    
+    var backgroundColor: UIColor {
+        switch self {
+            case .new:
+                return .systemGreen
+            case .best:
+                return .systemRed
+        }
+    }
+}
